89ee8fdd9f24d3dc34f09941bd842bd5bb7462ff
Bruce Becker <bruce.becker@uefa.ch>
2020-01-21 11:23:49 +0100

    Remove VPC components from main
    Signed-off-by: Bruce Becker <bruce.becker@uefa.ch>

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,34 +1,4 @@
+# cloud
 provider "aws" {
-  region = "eu-central-1"
-}
-
-resource "aws_vpc" "jenkins_master" {
-  cidr_block           = "192.168.1.0/24"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-
-  tags {
-    created_by = "Bruce"
-    name       = "Jenkins VPC"
-  }
-}
-
-# AWS Subnet in the VPC that Jenkins will deal with.
-resource "aws_subnet" "jenkins_master" {
-  availability_zone = "eu-central-1a"
-  cidr_block        = "192.168.1.0/24"
-  vpc_id            = "${aws_vpc.jenkins_master.id}"
-
-  tags {
-    created_by = "Bruce"
-    name       = "Jenkins subnet"
-  }
-}
-
-output "vpc_id" {
-  value = "${aws_vpc.jenkins_master.id}"
-}
-
-output "subnets" {
-  value = "${aws_subnet.jenkins_master.id}"
+  region = "${var.region}"
 }
